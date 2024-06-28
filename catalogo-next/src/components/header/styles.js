99cba1db7d57fd7a6f0e98c5f07891753e60dceb
felipe <carlosfelipemonte1997@hotmail.com>
2024-06-28 11:39:48 -0300

    Style: rename project

--- catalogo-next/src/components/header/styles.js
+++ catalogo-next/src/components/header/styles.js
@@ -11,8 +11,9 @@ export const ContainerHeader = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 20px 160px;
 
+  padding: 20px 160px;
+ 
   svg {
     width: 32px;
     height: 32px;
